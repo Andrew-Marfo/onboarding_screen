be0b97c9b5bb9181ec284b8816a2c7a1de2a8c73
Andrew Marfo <andrewmarfo23@gmail.com>
2024-04-02 22:47:45 +0000

    displaying animations

--- lib/intro_pages/into_page1.dart
+++ lib/intro_pages/into_page1.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class IntroPage1 extends StatelessWidget {
   const IntroPage1({super.key});
@@ -6,9 +7,24 @@ class IntroPage1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.lightBlueAccent,
-      child: const Center(
-        child: Text('Page 1'),
+      color: Colors.white,
+      child: Center(
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(32.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset('assets/Animation1.json'),
+                const SizedBox(
+                  height: 20,
+                ),
+                const Text(
+                    'sed viverra ipsum nunc aliquet bibendum enim facilisis gravida neque convallis a cras semper auctor neque vitae tempus quam pellentesque,'),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
